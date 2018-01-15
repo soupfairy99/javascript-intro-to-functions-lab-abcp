6737a9d72826fc37321cb4a0be50531bb483e546
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-15 05:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function shout(string) {
-return string.toUpperCase()
+function shout(string) {console.log( string.toUpperCase())
 }
-function whisper(string2) {
-return string2.toLowerCase()
+function whisper(string) {
+return string.toLowerCase()
 }
-function logShout(string3) {console.log('${logshout.toUpperCase()}')
+function logShout(string) {console.log( string.toUpperCase())
 }
+function logWhisper(string)
+{console.log( string.toLowerCase())
+}
+function sayHiToGrandma(string)
+{returns( string.toUpperCase())
+}
+   
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
