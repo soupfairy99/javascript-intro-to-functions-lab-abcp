bdac2d886c9a8189b33a7ec713942fbf7041e083
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-15 05:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ return string.toLowerCase()
 }
 function logShout(string) {console.log( string.toUpperCase())
 }
+logShout('hello')
 function logWhisper(string)
 {console.log( string.toLowerCase())
 }
